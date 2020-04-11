c093cf6e6515ae8f4eed2d88bfbac1f1cd8b882b
Paulo-Crisci <46006723+Paulo-Crisci@users.noreply.github.com>
2020-04-11 14:40:34 -0300

    Add files via upload

---
+++ validations/cpf/front-end/cpf.php
@@ -0,0 +1,40 @@
+<?php 
+
+
+ 	$cpf ='493.055.628-76';
+
+ 	function ocpf($cpf){
+			$cpf = preg_replace("/[^0-9]/", "", $cpf);
+			$digitoUm= 0;
+			$digitoDois = 0;
+
+		for($i = 0, $x = 10; $i <= 8; $i++, $x--){
+			$digitoUm += $cpf[$i] * $x;
+		}
+		
+		for($i = 0, $x = 11; $i <= 9; $i++, $x--){
+			if(str_repeat($i, 11) == $cpf){
+				return false;
+				}
+				 
+		$digitoDois += $cpf[$i] * $x;
+        }
+		
+		$calculoUm = (($digitoUm%11)<2) ? 0 : 11-($digitoUm%11);
+		$calculoDois = (($digitoDois%11)<2) ? 0 : 11-($digitoDois%11);
+		
+		if($calculoUm <> $cpf[9] || $calculoDois <> $cpf[10]){
+			return false;
+	}
+	return true;
+	}
+	
+	if(ocpf($cpf)){
+		
+	echo 'CPF válido';
+	}else{
+		echo 'CPF inválido';
+	}
+
+	
+ ?>
